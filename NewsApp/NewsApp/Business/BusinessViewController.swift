1b28515a4aa41559844ec7605b65ae0df9b05aef
Марина <77566874+marizhura@users.noreply.github.com>
2023-08-17 23:59:27 +0300

    lesson work

---
+++ NewsApp/NewsApp/Business/BusinessViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BusinessViewController.swift
+//  NewsApp
+//
+//  Created by Марина Журавлева on 16.08.2023.
+//
+
+import UIKit
+
+final class BusinessViewController: UIViewController {
+    
+    
+    // MARK: - GUI Variables
+    
+    // MARK: - Properties
+
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGray
+    }
+    
+    // MARK: - Methods
+
+    // MARK: - Private methods
+}
---
+++ NewsApp/NewsApp/Technology/TechnologyViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TechnologyViewController.swift
+//  NewsApp
+//
+//  Created by Марина Журавлева on 16.08.2023.
+//
+
+import UIKit
+
+final class TechnologyViewController: UIViewController {
+    
+    // MARK: - GUI Variables
+    
+    // MARK: - Properties
+
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+    
+    // MARK: - Methods
+
+    // MARK: - Private methods
+}
